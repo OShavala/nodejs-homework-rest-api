c5aaed001848b58e95ddd2f92d28fc1f74a9ecd9
Olena Shavala <olena.shvl@gmail.com>
2023-10-20 13:44:10 +0300

    first fixation

--- helpers/index.js
+++ helpers/index.js
@@ -1,11 +1,11 @@
 const HttpError = require("./HttpError");
-const ctrl = require("./ctrlWrapper");
+const ctrlWrapper = require("./ctrlWrapper");
 const handleMongooseError = require("./handleMongooseError");
-
 module.exports = {
   HttpError,
-  ctrl,
+  ctrlWrapper,
   handleMongooseError,
 };
+
 
 
